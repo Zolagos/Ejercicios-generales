478709ac3c46ba7790c05996b42b82d66e95f2ea
Zolagos <smmerino@outlook.com>
2026-03-29 23:33:16 -0500

    Update funtion_coder.py

--- s/funtion_coder.py
+++ s/funtion_coder.py
@@ -23,23 +23,63 @@ coders = {
     }
 }
 def menu ():
-    option = input("Write the option:\n 1.Add\n 2.Search\n 3.Remove\n 4.Exit\n")
-    option = option.lower().strip()
-    if option == "add":
-        id = int(input("Digite el ID: "))
-        nombre = input("Digite el nombre: ")
-        apellido = input("DIgite el apellido: ")
-        edad = int(input("Digite la edad: "))
-        nivel = input("Digite el nivel de ingles: ")
-        coders[c]= {"id":id,"nombre":nombre,"apellido":apellido,"edad":edad,"nvlIngles":nivel}
-        print(coders)
-    elif option == "search":
-        busqueda_id = int(input("Digite el ID que desea buscar: "))
-        coder_encontrado = "No existe"
-        for llave, datos in coders.items():
-            if datos["id"] == busqueda_id:
-                coder_encontrado = datos
-        print(coder_encontrado)
-    elif option == "remove":
-        rem = int(input("wich ID desea eleminar:\n"))
-        del (coders[rem])
+    option =""
+    while option !="exit":
+        option = input("Write the option:\n 1.Add\n 2.Search\n 3.Remove\n 4.Exit\n")
+        option = option.lower().strip()
+        if option == "add":
+            add(c)
+        elif option == "search":
+            search()
+        elif option == "remove":
+            remove()
+        elif option == "exit":
+            print("Exit menu")
+        else:
+            print("INVALID OPTION!!!\n")
+    
+
+def add(contador):
+        df = False
+        while not df:
+            try:
+                id = int(input("Write the ID: "))
+                nombre = input("Write the name: ")
+                apellido = input("Write the last name: ")
+                edad = int(input("Write the age: "))
+                nivel = input("Write the level of english: ")
+                contador+=1
+                coders[contador]= {"id":id,"nombre":nombre,"apellido":apellido,"edad":edad,"nvlIngles":nivel}
+                print(coders)
+                df = True
+            except:
+                print("INVALID INPUT!!!\n")
+                
+        return contador
+
+def search():
+    df = False
+    while not df:
+        try:
+            busqueda_id = int(input("Write the ID to search: "))
+            coder_encontrado = "The ID does not exit\n"
+            for llave, datos in coders.items():
+                if datos["id"] == busqueda_id:
+                    coder_encontrado = datos
+            print(coder_encontrado)
+            df = True
+        except:
+                print("INVALID INPUT!!!\n")
+
+def remove():
+    df = False
+    while not df:
+        try:
+            rem = int(input("Write the number of the regitry to remove:"))
+            del (coders[rem])
+            df = True
+        except:
+                print("INVALID INPUT!!!\n")
+
+
+menu()
